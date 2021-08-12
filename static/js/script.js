953468f95a2a4c789759cf358d19f9bb27fb39c7
ruszkipista <oi5672@gmail.com>
2021-08-12 08:58:16 +0200

    fix html error: attribute data-form-method at modal handling

--- static/js/script.js
+++ static/js/script.js
@@ -7,11 +7,11 @@ function connectModal() {
             // extract target modal from attribute data-mdb-target
             const targetModal = document.querySelector(source.dataset.mdbTarget);
             if (targetModal) {
-                // get the Button element with formMethod attribute
-                const targetButton = targetModal.querySelector('button[formMethod]');
+                // get the Button element with data-form-method attribute
+                const targetButton = targetModal.querySelector('button[data-form-method]');
                 targetButton.formAction = source.href;
                 targetButton.addEventListener('click', (event) => {
-                    fetch(event.target.formAction, {method:event.target.formMethod})
+                    fetch(event.target.formAction, {method:event.target.dataset["formMethod"]})
                     // learnt handling the returned whole page here https://gomakethings.com/getting-html-with-fetch-in-vanilla-js/
                     .then(result=> result.text())
                     .then(html => document.querySelector('html').innerHTML = html)
