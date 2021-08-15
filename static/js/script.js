d21f5cca59bcc1b7d6e436ca6a0995989f0ee9c5
ruszkipista <oi5672@gmail.com>
2021-08-15 11:21:30 +0200

    fix jshint vaidator errors

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // inspired by https://stackoverflow.com/questions/59566549/how-to-do-delete-confirmation-for-a-table-data-with-bootstrap-modal-in-django
 document.addEventListener('DOMContentLoaded', connectModal());
 function connectModal() {
-    const sources = document.querySelectorAll('a[data-mdb-toggle="modal"]')
+    const sources = document.querySelectorAll('a[data-mdb-toggle="modal"]');
     sources.forEach(source => {
         source.addEventListener("click", event => {
             // extract target modal from attribute data-mdb-target
@@ -11,7 +11,7 @@ function connectModal() {
                 const targetButton = targetModal.querySelector('button[data-form-method]');
                 targetButton.formAction = source.href;
                 targetButton.addEventListener('click', (event) => {
-                    fetch(event.target.formAction, {method:event.target.dataset["formMethod"]})
+                    fetch(event.target.formAction, {method:event.target.dataset.formMethod})
                     // learnt handling the returned whole page here https://gomakethings.com/getting-html-with-fetch-in-vanilla-js/
                     .then(result=> result.text())
                     .then(html => document.querySelector('html').innerHTML = html)
@@ -27,18 +27,16 @@ function selectTableRows(event){
     // the SELECT element has attributes data-filter, the child OPTION elements have value=<option-car_id>
     // the TR element has attributes data-filter, data-car_id_filter=<row-car_id>
     // a table row is displayed if TR matches all SELECT selected options
-    // this function is triggered by the SELECT-onChange event
-    const select = event.currentTarget;
-    
+    // this function is triggered by the SELECT-onChange event  
     const selectFilters = document.querySelectorAll('select[data-filter]');
     const trFilters = document.querySelectorAll('tr[data-filter]');
     trFilters.forEach(tr=>{
         let is_selected = true;
         selectFilters.forEach(e=>{
-            let selectedValue = e.options[e.selectedIndex].value
-            let rowValue = tr.dataset[e.id]
-            is_selected &&= selectedValue=="" || rowValue == selectedValue; 
-        })
+            let selectedValue = e.options[e.selectedIndex].value;
+            let rowValue = tr.dataset[e.id];
+            is_selected = is_selected && (selectedValue=="" || rowValue==selectedValue); 
+        });
         tr.style.display = (is_selected) ? "" : "none";
-    })
+    });
 }
